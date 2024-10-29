47dc71f5eeb22ba4e76427e0c66a749c43e73f93
erickjordanstm@gmail.com <erickjordanstm@gmail.com>
2024-10-29 15:48:42 -0300

    Primeiro commit

---
+++ Consulta.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConsultaCNPJ
+{
+    public class EmpresaCNPJ
+    {
+         public static EmpresaCNPJ ConsultaCNPJ(string CNPJ)
+        {
+            string URL = "https://www.receitaws.com.br/v1/cnpj/" + CNPJ;
+            WebClient Client = new WebClient();
+            Client.Encoding = Encoding.UTF8;
+            string joson = Client.DownloadString(URL);
+            var empresa = JsonConvert.DeserializeObject<EmpresaCNPJ>(joson);
+            return empresa;
+        }
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("ultima_atualizacao")]
+        public DateTimeOffset UltimaAtualizacao { get; set; }
+
+        [JsonProperty("cnpj")]
+        public string Cnpj { get; set; }
+
+        [JsonProperty("tipo")]
+        public string Tipo { get; set; }
+
+        [JsonProperty("porte")]
+        public string Porte { get; set; }
+
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+
+        [JsonProperty("fantasia")]
+        public string Fantasia { get; set; }
+
+        [JsonProperty("abertura")]
+        public string Abertura { get; set; }
+
+        [JsonProperty("atividade_principal")]
+        public Atividade[] AtividadePrincipal { get; set; }
+
+        [JsonProperty("atividades_secundarias")]
+        public Atividade[] AtividadesSecundarias { get; set; }
+
+        [JsonProperty("natureza_juridica")]
+        public string NaturezaJuridica { get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonProperty("numero")]
+        public string Numero { get; set; }
+
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty("municipio")]
+        public string Municipio { get; set; }
+
+        [JsonProperty("uf")]
+        public string Uf { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("telefone")]
+        public string Telefone { get; set; }
+
+        [JsonProperty("efr")]
+        public string Efr { get; set; }
+
+        [JsonProperty("situacao")]
+        public string Situacao { get; set; }
+
+        [JsonProperty("data_situacao")]
+        public string DataSituacao { get; set; }
+
+        [JsonProperty("motivo_situacao")]
+        public string MotivoSituacao { get; set; }
+
+        [JsonProperty("situacao_especial")]
+        public string SituacaoEspecial { get; set; }
+
+        [JsonProperty("data_situacao_especial")]
+        public string DataSituacaoEspecial { get; set; }
+
+        [JsonProperty("capital_social")]
+        public string CapitalSocial { get; set; }
+
+        [JsonProperty("qsa")]
+        public Qsa[] Qsa { get; set; }
+
+        [JsonProperty("billing")]
+        public Billing Billing { get; set; }
+    }
+    public partial class Atividade
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public partial class Billing
+    {
+        [JsonProperty("free")]
+        public bool Free { get; set; }
+
+        [JsonProperty("database")]
+        public bool Database { get; set; }
+    }
+
+    public partial class Qsa
+    {
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+
+        [JsonProperty("qual")]
+        public string Qual { get; set; }
+
+        [JsonProperty("pais_origem")]
+        public string PaisOrigem { get; set; }
+
+        [JsonProperty("nome_rep_legal")]
+        public string NomeRepLegal { get; set; }
+
+        [JsonProperty("qual_rep_legal")]
+        public string QualRepLegal { get; set; }
+    }
+}
---
+++ Sintegra.cs
@@ -0,0 +1,78 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Net.Http;
+//using HtmlAgilityPack;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+
+//namespace ConsultaCNPJ
+//{
+//    public class ConsultaSintegra
+//    {
+      
+//        public static void PreparaPagina(string CNPJ)
+//        {
+           
+
+
+//                IWebDriver driver = new ChromeDriver();
+
+//                try
+//                {
+//                    driver.Navigate().GoToUrl("https://app.sefa.pa.gov.br/sintegra/");
+
+//                    // Clica no checkbox "Não sou um robô"
+//                    IWebElement checkbox = driver.FindElement(By.ClassName("g-recaptcha"));
+//                    checkbox.Click();
+
+//                    // Preenche o campo CNPJ
+//                    IWebElement cnpjInput = driver.FindElement(By.Id("CNPJ"));
+//                    cnpjInput.SendKeys(CNPJ); // Substitua pelo CNPJ desejado
+
+//                    // Clica no botão "Consultar"
+//                    IWebElement consultarButton = driver.FindElement(By.ClassName("button"));
+//                    consultarButton.Click();
+//                string html = driver.PageSource;
+//                BuscaDados(html);
+//                    // Agora você pode extrair os dados da página conforme necessário
+//                    // ...
+
+//                    // Feche o navegador
+//                    driver.Quit();
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine($"Erro: {ex.Message}");
+//                }
+//            }
+
+
+//        public static  async void BuscaDados(string HTML)  //Alterar  o Tipo de dados 
+//        {
+//            var httpClient = new HttpClient();
+//            var url = "https://app.sefa.pa.gov.br/sintegra/";
+
+//            var response = await httpClient.GetAsync(url);
+//            var html = await response.Content.ReadAsStringAsync();
+
+//            var htmlDoc = new HtmlDocument();
+//            htmlDoc.Load(HTML.Length>0? HTML:html);
+
+//            // Use o HtmlAgilityPack para localizar os elementos relevantes
+//            // Extraia os dados e preencha a classe Empresa
+//            // ...
+
+//            // Exemplo de uso:
+
+//        }
+
+//    }
+
+
+
+
+//}
